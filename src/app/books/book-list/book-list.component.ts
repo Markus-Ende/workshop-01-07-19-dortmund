37ad8e43c8bf3195ffdf42de5353167ce2242e38
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-07-03 14:45:12 +0200

    view encapsulation

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { Book } from '../book';
 import { BookDataService } from '../book-data.service';
 import { Subscription } from 'rxjs';
@@ -6,7 +11,8 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class BookListComponent implements OnInit, OnDestroy {
   books: Book[];
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class NavigationComponent implements OnInit {
   constructor() {}
